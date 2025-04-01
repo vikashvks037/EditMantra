3b9d7a2042fc83d9b66a3607662f93aaf8e4d754
vikashvks037 <157198757+vikashvks037@users.noreply.github.com>
2025-04-01 23:53:09 +0530

    Update AdminEditorPage.jsx

--- editmantra-main/src/components/Admin/AdminEditorPage.jsx
+++ editmantra-main/src/components/Admin/AdminEditorPage.jsx
@@ -104,7 +104,7 @@ const AdminEditorPage = () => {
                     Leave
                 </button>
             </div>
-            <div className="editorWrap p-4">
+            <div className="editorWrap p-1">
                 <Editor
                     socketRef={socketRef}
                     roomId={roomId}
